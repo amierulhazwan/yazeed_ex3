d8c90575902b387a41b5b3e142c95346ef93b746
yazeed <yazeedwong@gmail.com>
2021-10-28 12:12:55 +0800

    lkdsadsahjdsakjhdsaakjdhsa

--- lib/main.dart
+++ lib/main.dart
@@ -3,25 +3,51 @@ import 'package:flutter/material.dart';
 import './dropdown.dart';
 import './home.dart';
 import 'dart:developer';
+import 'package:shared_preferences/shared_preferences.dart';
 
 void main() {
   runApp(MaterialApp(home: MyApp()));
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
   final idController = TextEditingController();
+
   final passController = TextEditingController();
+
   final emailController = TextEditingController();
 
   bool _isOff = true;
+
+  @override
+  void initState() {
+    idController.addListener(checkFormValidity);
+    passController.addListener(checkFormValidity);
+    emailController.addListener(checkFormValidity);
+    super.initState();
+  }
+
+  void checkFormValidity() {
+    setState(() {
+      if (idController.text.isEmpty ||
+          passController.text.isEmpty ||
+          emailController.text.isEmpty) {
+        _isOff = true;
+      } else {
+        _isOff = false;
+      }
+    });
+  }
 
   void submitData() {
     final enteredID = idController.text;
     final enteredPASS = passController.text;
     final enteredEMAIL = emailController.text;
-    if (enteredID.isEmpty || enteredPASS.isEmpty || enteredEMAIL.isEmpty) {
-      return;
-    }
+    if (enteredID.isEmpty || enteredPASS.isEmpty || enteredEMAIL.isEmpty) {}
 
     print('$enteredID');
   }
@@ -37,6 +63,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
+        resizeToAvoidBottomInset: false,
         backgroundColor: Colors.white,
         body: Column(
           children: <Widget>[
@@ -57,58 +84,51 @@ class MyApp extends StatelessWidget {
               child: Image.asset('assets/images/derib.png'),
             ),
             Card(
-              child: Container(
-                  padding: EdgeInsets.all(10),
-                  width: 300,
-                  child: Column(
+                child: Container(
+              padding: EdgeInsets.all(10),
+              width: 300,
+              child: Column(
+                children: [
+                  Container(
+                    child: TextField(
+                      decoration: InputDecoration(labelText: 'Name'),
+                      controller: idController,
+                      onSubmitted: (val) => submitData,
+                    ),
+                  ),
+                  TextField(
+                    decoration: InputDecoration(labelText: 'email'),
+                    controller: emailController,
+                    onSubmitted: (val) => submitData,
+                  ),
+                  TextField(
+                    decoration: InputDecoration(labelText: 'password'),
+                    controller: passController,
+                    onSubmitted: (val) => submitData,
+                  ),
+                  Row(
                     children: [
-                      TextField(
-                        decoration: InputDecoration(labelText: 'Name'),
-                        controller: idController,
-                        onSubmitted: (val) => submitData,
-                      ),
-                      TextField(
-                        decoration: InputDecoration(labelText: 'email'),
-                        controller: emailController,
-                        onSubmitted: (val) => submitData,
-                      ),
-                      TextField(
-                        decoration: InputDecoration(labelText: 'password'),
-                        controller: passController,
-                        onSubmitted: (val) => submitData,
-                      ),
-                      ValueListenableBuilder<TextEditingValue>(
-                          valueListenable: passController,
-                          builder: (context, value, child) {
-                            return /* Stack(
-                            children:[ Positioned(
-                              top:50,
-                              child:  */
-                                ElevatedButton(
-                              onPressed: value.text.isNotEmpty
-                                  ? () {
-                                      Navigator.push(
-                                        context,
-                                        MaterialPageRoute(
-                                            builder: (context) => home()),
-                                      );
-                                    }
-                                  : null,
-                              child: Text('SIGN UP'),
-                            );
-                            /* ),
-                            ]); */
-                          }),
-                      Row(
-                        children: [
-                          Text('Choose gender: ',
-                              style: TextStyle(color: Colors.black)),
-                          DropDownDemo(),
-                        ],
-                      ),
+                      Text('Choose gender: ',
+                          style: TextStyle(color: Colors.black)),
+                      DropDownDemo(),
                     ],
-                  )),
-            ),
+                  ),
+                  ElevatedButton(
+                          onPressed: !_isOff
+                              ? () {
+                                  Navigator.push(
+                                    context,
+                                    MaterialPageRoute(
+                                        builder: (context) => home()),
+                                  );
+                                }
+                              : null,
+                          child: Text('SIGN UP'),
+                        ),
+                ],
+              ),
+            )),
+
             // ElevatedButton(
             //     child: Text(_isOff ? 'Sign up':'Sign Up',
             //         style: TextStyle(color: Colors.white)),
